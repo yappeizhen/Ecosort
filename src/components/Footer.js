8c550f568d2b112b7f6476146a37e1c2a3d78a9a
yappeizhen <yappeizhen@gmail.com>
2024-04-14 17:25:54 +0800

    removed unused assets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,22 @@
 // Import dependencies
 import React from "react";
 import styled from "styled-components";
-import tfjsLogo from "../assets/tensorflow-js-logo-social.png";
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  align-items: center;
   font-family: "Poppins";
-  padding: 12px 0;
   width: 100%;
   background-color: rgb(240, 248, 255, 0.7);
   bottom: 0;
   margin: 0;
-`;
-const StyledLogo = styled.img`
-  width: 160px;
-  @media only screen and (max-width: 768px) {
-    width: 100px;
-  }
-`;
-const StyledText = styled.div`
-  margin-right: 12px;
-  @media only screen and (max-width: 768px) {
-    font-size: 12px;
-  }
+  font-size: 11px;
+  padding: 0 8px;
 `;
 function Footer() {
   return (
     <>
       <StyledContainer>
-        <StyledText>Powered by:</StyledText>
-        <StyledLogo src={tfjsLogo} alt="tfjs-logo" />
+        Made with ❤️ by Aloysius, Pei Zhen, Saga, Terence, Vinicius
       </StyledContainer>
     </>
   );
