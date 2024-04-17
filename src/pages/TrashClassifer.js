f02166c02dadfd2494e430e9ddde05ab950580d7
yappeizhen <yappeizhen@gmail.com>
2024-04-17 15:31:34 +0800

    Revert "updated model url"
    This reverts commit 45961eaa16544654b7971f87486da1b5a6010f51.

--- src/pages/TrashClassifer.js
+++ src/pages/TrashClassifer.js
@@ -15,7 +15,7 @@ function TrashClassifer() {
       }
       classBank={classBank}
       modelUrl={
-        "https://raw.githubusercontent.com/yappeizhen/Ecosort/main/models/tfjs_model/model.json"
+        "https://github.com/yappeizhen/Ecosort/blob/main/models/tfjs_model/model.json"
       }
     />
   );
